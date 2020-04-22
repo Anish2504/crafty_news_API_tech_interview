f008435a66a2314153cd7149ae9b3b1489a15d51
Anish2504 <anish.kanswal@gmail.com>
2020-04-22 16:03:59 +0200

    added test for happy path

--- spec/requests/api/comments/user_can_create_comments_spec.rb
+++ spec/requests/api/comments/user_can_create_comments_spec.rb
@@ -1,4 +1,24 @@
-RSpec.describe 'POST /api/comments/:id', type: :request do
+RSpec.describe 'POST /api/comments', type: :request do
+
+  let(:user_credentials) { user.create_new_auth_token }
+
+  describe 'POST/api/comments' do
+    before do
+      post '/api/events',
+        params: {
+          comments: {
+            body: 'This is a nice article!'
+          }
+        }
+        header: user_headers
+
+      end
+
+      it 'should return a 200 response' do
+        expect(JSON.parse(response.body)['message'].to eq 'Comment was successfully created'
+      end
+    end
+
 
   
   
